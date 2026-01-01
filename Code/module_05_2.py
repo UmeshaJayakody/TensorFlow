eac41a48eb2a61dffa70897b2a195ab37aaf9002
UmeshaJayakody <umesha.22@cse.mrt.ac.lk>
2026-01-01 10:26:38 +0530

    Add cats vs dogs dataset loading and visualization example

---
+++ Code/module_05_2.py
@@ -0,0 +1,24 @@
+#Imports
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+keras = tf.keras
+
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+# split the data manually into 80% training, 10% testing, 10% validation
+(raw_train, raw_validation, raw_test), metadata = tfds.load(
+    'cats_vs_dogs',
+    split=['train[:80%]', 'train[80%:90%]', 'train[90%:]'],
+    with_info=True,
+    as_supervised=True,
+)
+get_label_name = metadata.features['label'].int2str  # creates a function object that we can use to get labels
+
+# display 2 images from the dataset
+for image, label in raw_train.take(5):
+  plt.figure()
+  plt.imshow(image)
+  plt.title(get_label_name(label))
